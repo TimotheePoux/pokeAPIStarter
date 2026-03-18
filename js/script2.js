1b1e90893e4877ba71bfe378cdee70381526f346
Timothée Poux <timothee.poux@gmail.com>
2026-03-18 11:32:49 +0100

    ajout de commentaires

--- js/script2.js
+++ js/script2.js
@@ -1,44 +1,42 @@
-let apiLink = "https://tyradex.app/api/v1/pokemon"; 
-const data = await fetch (apiLink)
+let apiLink = "https://tyradex.app/api/v1/pokemon"; //définition du lien de l'API
+const data = await fetch (apiLink) //récupère l'api
     .then(response => response.json())
     .catch(error => alert("Erreur : " + error));
 
-console.log('Données récupérées :', data);
+console.log('Données récupérées :', data); //vérifie que les donnés sont bien récupérées
 
-const dataClean = data.filter(pokemon => pokemon.types != null);
-let gen = 1;
-let dataSorted = dataClean.filter(pokemon => pokemon.generation == gen);
+const dataClean = data.filter(pokemon => pokemon.types != null); //nouvelle liste sans pokemon bug
+let gen = 1; //cette variable gen facilite le tri avec les boutons de type
+let dataSorted = dataClean.filter(pokemon => pokemon.generation == gen); //variable triée qui sera affichée sur la page,quand on ouvre la page, la génération sélectionnée par défaut est la gen 1
 let selecteur = document.querySelector('select');
-console.log(selecteur.value)
 selecteur.addEventListener('click', ()=>{
-    console.log(selecteur.value);
-    gen = selecteur.value;
-    dataSorted = dataClean.filter(pokemon => pokemon.generation == gen);
-    loadPKMN();
+    gen = selecteur.value; //màj de la variable gen
+    dataSorted = dataClean.filter(pokemon => pokemon.generation == gen);//tri selon la génération sélectionnée
+    loadPKMN();//réaffiche les pokémons
 });
 
 let typeButtons = document.querySelectorAll('.btnType');
 typeButtons.forEach(typeButton =>{
-    typeButton.style.backgroundColor = getTypeColor(typeButton.value);
+    typeButton.style.backgroundColor = getTypeColor(typeButton.value);//applique à chaque bouton de type la couleur qui lui est associée
     typeButton.addEventListener('click', ()=>{
-    dataSorted = [];
+    dataSorted = [];//vide dataSorted pour la re-remplir par la suite
     dataClean.forEach(pokemon=>{
-        if (pokemon.generation == gen){
+        if (pokemon.generation == gen){//sélectionne uniquement les pokémons présents dans la génération sélectionnée
             if (pokemon.types[0].name == typeButton.value){
                 dataSorted.push(pokemon);
             }
-            else if(pokemon.types.length==2){
+            else if(pokemon.types.length==2){//condition nécessaire dans l'éventualité où on a un double typé
                 if (pokemon.types[1].name == typeButton.value){
                     dataSorted.push(pokemon);
                 }
             }
         }
     })
-    loadPKMN();
+    loadPKMN();//réaffiche les pokémons
 });
 });
 
-function getTypeColor(typeName) {
+function getTypeColor(typeName) {//Prend en entrée un nom de type et renvoie la couleur qui lui est associée
     switch (typeName) {
         case 'Plante':
             return '#4caf50';
@@ -80,19 +78,19 @@ function getTypeColor(typeName) {
 }
 
 const main = document.querySelector('main');
-loadPKMN();
+loadPKMN(); //Affiche les pokémons quand on ouvre la page
 
-function loadPKMN(){
-    main.innerHTML = "";
+function loadPKMN(){ //Affiche tous les pokémons contenus dans une liste de pokémon
+    main.innerHTML = "";//enlève tous les pokémons de la page pour re-remplir ensuite la page avec la nouvelle liste
     dataSorted.forEach(pokemon => {
-        const article = document.createElement('article');
-            if (pokemon.types.length == 2){
+        const article = document.createElement('article');//crée un article dans lequel on va mettre toutes les informations du pkmn
+            if (pokemon.types.length == 2){//si le pokémon à un double type, la bordure de sa carte aura la couleur de son deuxieme type, sinon elle aura la couleur de son premier (et seul) type
                 article.style.borderColor = getTypeColor(pokemon.types[1].name);
             }
             else{
                 article.style.borderColor = getTypeColor(pokemon.types[0].name);
             }
-        article.style.backgroundColor = getTypeColor(pokemon.types[0].name);
+        article.style.backgroundColor = getTypeColor(pokemon.types[0].name);//mets comme couleur de fond de la carte la couleur associée au premier type du pkmn
         article.innerHTML = `
             <figure>
                 <picture>
@@ -113,7 +111,7 @@ function loadPKMN(){
                     </ol>
                 </figcaption>
             </figure>
-        `;
-        main.appendChild(article);
+        `;//remplit l'article avec les infos du pokémon
+        main.appendChild(article);//ajoute l'article au main
     });
 }
